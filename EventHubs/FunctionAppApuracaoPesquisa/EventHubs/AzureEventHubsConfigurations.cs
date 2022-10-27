16a778b5383e208f1d3292f7864ee9a6d6550065
Renato Groffe <renato.groffe@yahoo.com.br>
2022-10-27 20:46:06 -0300

    Commit inicial
    Commit inicial

---
+++ EventHubs/FunctionAppApuracaoPesquisa/EventHubs/AzureEventHubsConfigurations.cs
@@ -0,0 +1,7 @@
+namespace FunctionAppApuracaoPesquisa.EventHubs;
+
+public static class AzureEventHubsConfigurations
+{
+    public const string EventHubName = "topic-votacaointeresse";
+    public const string ConsumerGroup = "serverless";
+}
